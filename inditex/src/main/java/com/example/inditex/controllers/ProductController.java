e6498dfd89fe08f0cb135ec1ecebe12bcc5612a4
Lorenzo Rodríguez <lorenzorodriguez@Lorenzos-MBP.home>
2022-10-18 20:41:52 +0200

    Set proper HTTP codes for responses

--- inditex/src/main/java/com/example/inditex/controllers/ProductController.java
+++ inditex/src/main/java/com/example/inditex/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.example.inditex.exceptions.HttpClientException;
 import com.example.inditex.services.ProductService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,19 +19,22 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("product/{productId}/similar")
-    public String getDetailsOfSimilarProducts(@PathVariable String productId) {
+    public String getDetailsOfSimilarProducts(@PathVariable String productId, HttpServletResponse response) {
         try {
             var products = productService.getDetailsOfSimilarProducts(productId);
             if (products.isEmpty()) {
                 HttpClientException productNotFound = new HttpClientException(404, "Not found");
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return returnError(productNotFound);
             } else {
                 return new ObjectMapper().writeValueAsString(products);
             }
         } catch (HttpClientException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             return returnError(e);
         } catch (JsonProcessingException e) {
             System.err.println("Error parsing to JSON: " + e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             throw new RuntimeException(e);
         }
     }
