d55789177120983c889696c88b5704f068570e5e
Lorenzo Rodríguez <lorenzorodriguez@Lorenzos-MBP.home>
2022-10-18 21:56:50 +0200

    Remove unused imports

--- inditex/src/test/java/com/example/inditex/controllers/ProductControllerTests.java
+++ inditex/src/test/java/com/example/inditex/controllers/ProductControllerTests.java
@@ -10,12 +10,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
